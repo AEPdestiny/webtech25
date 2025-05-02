3db763173b40613ef9dae6950c486daf4cf1fe93
Ibrahim Danisman <121925671+AEPdestiny@users.noreply.github.com>
2025-05-02 14:35:47 +0200

    probleme im frontend bestehen

--- Webtech25/src/main/java/com/Webtech25/Webtech25/Webtech25Application.java
+++ Webtech25/src/main/java/com/Webtech25/Webtech25/Webtech25Application.java
@@ -1,7 +1,9 @@
 package com.Webtech25.Webtech25;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Webtech25Application {
@@ -10,4 +12,11 @@ public class Webtech25Application {
 		SpringApplication.run(Webtech25Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(KleidungRepository kleidungRepo) {
+		return args -> {
+			kleidungRepo.save(new Kleidung("T-Shirt", "L","Lager A"));
+			kleidungRepo.save(new Kleidung("Hose", "M","Lager B"));
+		};
+	}
 }
