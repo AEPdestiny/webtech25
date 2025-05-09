e8ddaea92a3dea5dcc302f6439a7d48d483a4a8f
Ibrahim Danisman <121925671+AEPdestiny@users.noreply.github.com>
2025-05-09 10:59:58 +0200

    edit im Controller : "@RequestMapping("/kleidung")"

--- Webtech25/src/main/java/com/Webtech25/Webtech25/KleidungController.java
+++ Webtech25/src/main/java/com/Webtech25/Webtech25/KleidungController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/kleidung")
+@RequestMapping("/kleidung")
 public class KleidungController {
 
     private  KleidungRepository kleidungRepo;
